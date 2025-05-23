511d0418e30c94bd72396c927954620ad1606821
tomimandalaputra <tommymandalaputra@gmail.com>
2025-05-23 15:22:17 +0700

    chore: define viewModel CartManager

--- Toserba/Managers/CartManager.swift
+++ Toserba/Managers/CartManager.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+@Observable
+class CartManager {
+   var productsInCart: [ProductInCart] = []
+
+   func addToCart(product: Product) {
+      if let indexOfProductInCart = productsInCart.firstIndex(where: { $0.id == product.id }) {
+         let currentQuantity = productsInCart[indexOfProductInCart].quantity
+         let newQuantity = currentQuantity + 1
+
+         productsInCart[indexOfProductInCart] = ProductInCart(product: product, quantity: newQuantity)
+      } else {
+         productsInCart.append(ProductInCart(product: product, quantity: 1))
+      }
+   }
+}
