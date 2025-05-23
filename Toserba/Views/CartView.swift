d91841315bab5ab2f7b4e067ae6358d8d94913d3
tomimandalaputra <tommymandalaputra@gmail.com>
2025-05-23 15:23:08 +0700

    chore: slicing UI CartView

--- Toserba/Views/CartView.swift
+++ Toserba/Views/CartView.swift
@@ -8,11 +8,65 @@
 import SwiftUI
 
 struct CartView: View {
+   @Environment(CartManager.self) var viewModel: CartManager
+
+   fileprivate func CartRow(productInCart: ProductInCart) -> some View {
+      HStack {
+         AsyncImage(url: URL(string: productInCart.product.image)) { phase in
+            switch phase {
+               case .empty:
+                  ZStack {
+                     Rectangle()
+                        .fill(Color.surfaceCard)
+                        .frame(width: 70, height: 70)
+
+                     ProgressView().tint(Color.textMain)
+                  }
+
+               case .success(let image):
+                  image
+                     .squareImageModifier()
+
+               case .failure:
+                  ZStack {
+                     Rectangle()
+                        .fill(Color.surfaceCard)
+                        .frame(width: 70, height: 70)
+
+                     Image(systemName: "photo")
+                  }
+
+               @unknown default:
+                  EmptyView()
+            }
+         }
+
+         VStack(alignment: .leading) {
+            Text(productInCart.product.title)
+               .font(.system(size: 15, weight: .bold))
+               .padding(.bottom, 1)
+
+            Text("$\(productInCart.product.price, specifier: "%.2f")")
+               .font(.system(size: 18, weight: .semibold))
+               .foregroundStyle(Color.textMain)
+
+            Stepper("Quantity \(productInCart.quantity)") {} onDecrement: {}
+         }
+      }
+   }
+
    var body: some View {
-      Text("Cart")
+      AppScaffold {
+         VStack {
+            List(viewModel.productsInCart) { productInCart in
+               CartRow(productInCart: productInCart)
+            }
+         }
+      }
    }
 }
 
 #Preview {
    CartView()
+      .environment(CartManager())
 }
